83c5dd1f56be8d0041760eba6cbe3d119c1c1d79
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-03 18:35:52 -0400

    updating files

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,10 +45,10 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """ Function that returns a number even|odd """
-    if n%2 == 0:
-        return render_template('6-number_odd_or_even.html', num=n, even='even')
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html', num=n, odd_or_even='even')
     else:
-        return render_template('6-number_odd_or_even.html', num=n, even='odd')
+        return render_template('6-number_odd_or_even.html', num=n, odd_or_even='odd')
 
 
 if __name__ == '__main__':
