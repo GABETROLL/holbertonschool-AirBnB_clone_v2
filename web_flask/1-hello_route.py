7379c6ee32dc41d139f54151c6d1b9d69038ae53
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-04 09:45:49 -0400

    Add exercise 1 and update 0's documentation

--- web_flask/1-hello_route.py
+++ web_flask/1-hello_route.py
@@ -2,7 +2,8 @@
 """
 Flask website with plain text
 (without paragraph tags)
-that says "Hello, HBNB!".
+that says "Hello, HBNB!" in the main page,
+and "HBNB" in the path: /hbnb.
 
 When this script is the main script,
 the website is ran and hosted in
@@ -16,11 +17,19 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
-    Just "Hello, HBNB!"
-    without any tags sorrounding it
+    Just "Hello, HBNB!",
+    without any tags sorrounding it.
     """
     return "Hello HBNB!"
 
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """
+    Just "HBNB",
+    without any tags sorrounding it.
+    """
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
